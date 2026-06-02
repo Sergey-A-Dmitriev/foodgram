d72aae7933fdfeba5b073e3566ad3125ed01d13c
Sergey Dmitriev <sadmitriev.nv86@yandex.ru>
2026-06-03 02:12:11 +0500

    fix bag pep

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -5,4 +5,4 @@ from recipes.views import short_link_redirect
 urlpatterns = [
     path('s/<int:recipe_id>/',
          short_link_redirect,
-         name='recipe-short-link',),]
+         name='recipe-short-link',)]
